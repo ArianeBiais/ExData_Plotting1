89a351667cebbb0a218589e543271e814eeace89
ArianeBiais <ariane.biais@edhec.com>
2020-08-11 13:31:48 +0200

    final

--- Code_plot4.R
+++ Code_plot4.R
@@ -26,7 +26,7 @@ with(finaldata,plot(DateTime,Global_active_power,xlab="",ylab="Global Active Pow
 with(finaldata,plot(DateTime,Sub_metering_1,xlab="",ylab="Energy sub metering",type='l'))
 points(finaldata$DateTime,finaldata$Sub_metering_3,col="blue",type="l")
 points(finaldata$DateTime,finaldata$Sub_metering_2,col="red",type="l")
-legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=2,cex=0.5)
+legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=2,cex=0.2)
 with(finaldata,plot(DateTime,Voltage,xlab="datatime",ylab="Voltage",type="l"))
 with(finaldata,plot(DateTime,Global_reactive_power,xlab="datatime",ylab="Global_reactive_power",type="l"))
 dev.copy(png,"plot4.png",width=480,height=480)
